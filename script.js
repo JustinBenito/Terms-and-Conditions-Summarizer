9987c3d2e452674c3ffd10a3a59d34e3aeaa840a
JustinBenito <Justinbenito1974@gmail.com>
2023-02-12 00:23:16 +0530

    Working Logic with UI

--- script.js
+++ script.js
@@ -1,58 +1,53 @@
-var submit=document.getElementById("sub")
-submit.addEventListener("click", fireapi)
-const apiKey = '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';
-// function fireapi(){
-//     var content=document.getElementById("input").value;
-//     const apiKey = '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';
-//     const prompt = content+'\n\nTl;dr';
-
-//     fetch('https://api.openai.com/v1/completions', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Authorization': `Bearer ${apiKey}`,
-//   },
-//   body: JSON.stringify({
-//     model: 'text-davinci-003',
-//     prompt,
-//     temperature: 0.7,
-//     max_tokens: 60,
-//     top_p: 1.0,
-//     frequency_penalty: 0.0,
-//     presence_penalty: 1
-//   })
-// })
-// .then(response => response.json())
-// .then(data => datum=data)
-// .catch(error => console.error(error));
-
-// }
-
+var datum = "";
 const fireapi = async () => {
-    var content=document.getElementById("input").value;
-    const prompt = content+'\n\nTl;dr';
-    const result = await fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${apiKey}`,
-        },
-        body: JSON.stringify({
-          model: 'text-davinci-003',
-          prompt,
-          temperature: 0.7,
-          max_tokens: 1000,
-          top_p: 1.0,
-          frequency_penalty: 0.0,
-          presence_penalty: 1
-        })
-      });
+  console.log("Fired");
+  var content = document.getElementById("input").value;
+  const prom = content + "\n\nTl;dr";
+  const result = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer 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`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: prom,
+      temperature: 0.7,
+      max_tokens: 1000,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 1,
+    }),
+  });
   //the resultant json is stored into the data
   const data = await result.json();
-  return data;
-  };
-  
-    
-  const datum = await fireapi();
-  var summary=datum.choices.text;
-  summary!=null? document.getElementById("input").value=summary:document.getElementById("status").innerHTML+="<h1>Error Occured...</h1>";
+  // const result = await fetch("https://api.openai.com/v1/engines/tldr/jobs", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: `Bearer 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`,
+  //   },
+  //   body: JSON.stringify({
+  //     prom,
+  //   }),
+  // });
+
+  // // The resultant JSON is stored into the data
+  // const data = await result.json();
+
+  // Extract the summary from the response
+  const summary = data.choices[0].text;
+  console.log(summary);
+  datum = summary;
+  document.getElementById("input").value = "Points" + summary;
+  document.getElementById("status").innerHTML += "<h1>Done</h1>";
+  return summary;
+};
+
+document.getElementById("sub").addEventListener("click", fireapi);
+
+var summary = datum;
+// summary != null
+//   ? (document.getElementById("input").value = summary)
+//   : (document.getElementById("status").innerHTML +=
+//       "<h1>Error Occured...</h1>");
